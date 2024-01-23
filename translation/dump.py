d02fac8983f3553be80078309c6c0879840a8f83
sahakintan <145264080+sahakintan@users.noreply.github.com>
2024-01-24 00:35:18 +0530

    Script for overwriting HumanEval json file (#58)
    * Script for overwriting the original HumanEval json file
    
    * Create HumanEval json conversion script
    
    * Update Hinglish_Json
    
    * rename and move file
    
    * fixes to script
    
    * more fixes to script
    
    * HinglishEval!
    
    ---------
    
    Co-authored-by: Mrigank Pawagi <mrigankpawagi@gmail.com>

---
+++ translation/dump.py
@@ -0,0 +1,47 @@
+import json
+
+
+def extract_prompt_from_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Split the content using triple double quotes
+    parts = content.split('"""')
+
+    return '"""\n    ' + parts[3].strip() + '\n    """'
+
+
+def hinglish_json():
+
+    # File path to the JSON file
+    file_path = '../HumanEval.json'
+
+    # Read existing JSON data from the file
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    # modify the data with the hinglish prompts
+    new_data = []
+    for item in data:        
+        i = item['task_id'].split('/')[1]
+        prompt = extract_prompt_from_file(f'drafts/{i}') # original prompt
+        
+        prev_prompt = item['prompt'] # changing only the docstring not the function signature
+        
+        entry_point = item['entry_point']
+
+        if '"""' in prev_prompt:
+            index = prev_prompt.index('"""', prev_prompt.index(entry_point))
+        elif "'''" in prev_prompt:
+            index = prev_prompt.index("'''", prev_prompt.index(entry_point))
+
+        #overwriting the orginal prompt. also if the new docstring is longer than the slice it isnt an issue as the string length is changed in-place
+        item['prompt'] = item['prompt'][:index] + prompt
+        
+        new_data.append(item)
+
+    #write the new data to the json file
+    with open("../HinglishEval.json", 'w') as json_file:
+        json.dump(data, json_file)
+
+hinglish_json()
