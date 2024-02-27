8794e815bba233449a8544f7f103084808bceda6
Anirudh Gupta <146579014+AnirudhG07@users.noreply.github.com>
2024-02-28 01:30:42 +0530

    some more changes

--- generated_codes/unsanitized_codes/gpt_3.5_turbo_codes/159.py
+++
@@ -1,9 +0,0 @@
-def eat(number, need, remaining):
-    total_gajar = number + remaining
-    if total_gajar >= need:
-        eaten_gajar = need
-        remaining_gajar = total_gajar - need
-    else:
-        eaten_gajar = total_gajar
-        remaining_gajar = 0
-    return [eaten_gajar, remaining_gajar]
--- generated_codes/unsanitized_codes/gpt_4_codes/134.py
+++
@@ -1,6 +0,0 @@
-```python
-def check_if_last_char_is_a_letter(txt):
-    if txt and txt[-1].isalpha() and (len(txt) == 1 or txt[-2] == ' '):
-        return True
-    return False
-```
