ac87d6e81d75631d90776bcfd9067844a54cc134
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-07-06 23:32:47 +0530

    scripts for codemodels
    These are raw scripts for codemodels, more will be added later on.

---
+++ samples/codemodels/codegen2B-mono.py
@@ -0,0 +1,14 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-2B-mono")
+model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-2B-mono", trust_remote_code=True)
+
+def codegen_2B_mono(prompt):
+    input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    generated_ids = model.generate(input_ids, max_length=256, pad_token_id=50256, temperature=1)
+    print(tokenizer.decode(generated_ids[0]))
+
+if __name__ == "__main__":
+        prompt="def prime(num:int): \"\"\" Check if given number is prime or not \"\"\" "
+        codegen_2B_mono(prompt)
