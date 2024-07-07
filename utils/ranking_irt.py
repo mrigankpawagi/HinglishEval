f7ceabac27665e044936810c842660d47d78f46a
SidZRed <siddharthrr@iisc.ac.in>
2024-07-07 21:49:39 +0530

    Change import, styling and some minor logic.

--- utils/ranking_irt.py
+++ utils/ranking_irt.py
@@ -1,12 +1,12 @@
 import numpy as np
-from girth import *
+from girth import twopl_mml
 
 def irt_ranking(data, models):
     '''
     This function ranks the models based on their scores calculated using the 2PL model.
 
     :param data: A 2D numpy array where each row represents a model and each column represents a question.
-                data[i][j] = 1 if model i answered question j correctly, 0 otherwise.
+                data[i][j] = 1 if question i is answered by model j correctly, 0 otherwise.
     :param models: A list of strings where each string represents a model.
     '''
     
@@ -21,11 +21,11 @@ def irt_ranking(data, models):
     normalized_difficulties = (difficulty - min_difficulty) / (max_difficulty - min_difficulty)
 
     model_scores = []
-    for i in range(len(models)):
+    for j in range(len(models)):
         score = 0
-        for j in range(len(models[i])):
-            score += data[i][j] * normalized_difficulties[j]
-        model_scores.append((models[i], score))
+        for i in range(len(data)):
+            score += data[i][j] * normalized_difficulties[i]
+        model_scores.append((models[j], score))
 
     # Return the ranking of the models based on their scores.
     return sorted(model_scores, key=lambda x: x[1], reverse=True)
