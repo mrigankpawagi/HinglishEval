3f43962ec138a674de7c616bf070b12600895fd9
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-09-27 12:16:24 +0530

    utils irt_eval code edits for path

--- utils/irt_eval.py
+++ utils/irt_eval.py
@@ -1,7 +1,12 @@
-from pyirt import irt
 import os
 
-def compute_irt_params(data: list):
+from pyirt import irt
+
+from binary_matrix import irt_data as irt_english
+from binary_matrix import irt_data2 as irt_hinglish
+
+
+def compute_irt_params(irt_data: list):
     """
     This function is used to compute the Item(problem) and User(model) parameters using the IRT approach.
     :param data: A list of tuples in the format (user_id, item_id, response)
@@ -9,21 +14,29 @@ def compute_irt_params(data: list):
     item_id - ID of the item.
     response - Binary response of the user to the item.
     """
-    item_param, user_param = irt(data)
+    item_param, user_param = irt(irt_data)
     return item_param, user_param
+
 
 if __name__ == "__main__":
     data = []
-    models =[]
-    for model in os.listdir("/samples/sanitized/"):
-        models.append(model)
-        for pid in os.listdir(f"/samples/sanitized/{model}"):
-            with open(f"/samples/sanitized/{model}/{pid}.py", "r") as f:
+    models = []
+    # get the filepath of tihs code
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    for lang in ["Hinglish", "English"]:
+        irt_matrix = irt_english if lang == "English" else irt_hinglish
+        sanitized_code_dir = os.path.join(current_dir, f"../samples/{lang}/sanitized")
+        for model in os.listdir(sanitized_code_dir):
+            model = model.replace(".zip", "") if model.endswith(".zip") else model
+            if model in ["codegen2_1B"]:
+                continue
+            models.append(model)
+            for pid in range(164):
                 response = irt_matrix[model][pid]
                 data.append((model, pid, response))
-    
-    item_param, user_param = compute_irt_params(data)
-    print(item_param)
-    print(user_param)
+
+        item_param, user_param = compute_irt_params(data)
+        print(item_param)
+        print(user_param)
     # Use the user_param to obtain the latency of the models.
     # Use the item_param to obtain the difficulty and discriminations of the problems.
