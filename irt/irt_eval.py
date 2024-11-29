48aae3b2f4fb10bbd66ba377bef97b09f8c38165
SidZRed <siddharthrr@iisc.ac.in>
2024-11-29 22:18:29 +0530

    Add requirements and refactor irt_eval.py

--- irt/irt_eval.py
+++ irt/irt_eval.py
@@ -13,17 +13,20 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 def compute_irt_params(irt_data: list):
     """
     This function is used to compute the Item(problem) and User(model) parameters using the IRT approach.
-    :param data: A list of tuples in the format (user_id, item_id, response)
-    user_id - ID of the user.
-    item_id - ID of the item.
-    response - Binary response of the user to the item.
+    :param irt_data : list : A list of tuples containing the model name, problem id and the binary response of the model to the problem.
     """
     item_param, user_param = irt(irt_data)
     return item_param, user_param
 
 
 def lang_irt():
-    # get the filepath of tihs code
+    """
+    Script to compute the IRT parameters for the models in both Hinglish and English. 
+    Here both language models are considered in independent IRT evaluations.
+    The user and item parameters are stored in a csv file. (./irt_ratings/{lang}_irt_userparams.csv, ./irt_ratings/{lang}_irt_itemparams.csv)
+    User parameters : Latency values of the models.
+    Item parameters : Difficulty and Discrimination values of the problems.
+    """
     for lang in ["Hinglish", "English"]:
         models = []
         data = []
@@ -45,7 +48,7 @@ def lang_irt():
 
         # with open(f"./irt_ratings/{lang}_irt_itemparams.json", "w") as file:
         #    file.write(str(item_param))
-        # add user_param in json formatted style in a new file, user_param.json
+
         csv_data = [["MODEL", "VALUE"]]
         for model, value in user_param.items():
             csv_data.append([model, f"{value:.3f}"])
@@ -55,7 +58,7 @@ def lang_irt():
             writer = csv.writer(file)
             writer.writerows(csv_data)
 
-            ## Item Parameters
+        ## Item Parameters
         csv_data = [["PROBLEM", "DIFFICULTY", "DISCRIMINATION"]]
         for problem, value in item_param.items():
             csv_data.append([problem, f"{value['beta']:.3f}", f"{value['alpha']:.3f}"])
@@ -70,12 +73,12 @@ def lang_irt():
         ) as file:
             writer = csv.writer(file)
             writer.writerows(csv_data)
-    # Use the user_param to obtain the latency of the models.
-    # Use the item_param to obtain the difficulty and discriminations of the problems.
 
 
 def combined_irt():
-    ## COMBINED
+    """
+    This is for computing the IRT parameters for the models in both Hinglish and English combined in a single evaluation.
+    """
     data = []
     models = []
     sanitized_code_dir = os.path.join(current_dir, "../samples/English/sanitized")
@@ -95,6 +98,7 @@ def combined_irt():
                 data.append((model + "_" + lang, pid, response))
 
     item_param, user_param = compute_irt_params(data)
+    
     # with open("./irt_ratings/Combined_irt_userparams.json", "w") as file:
     #    file.write(str(user_param))
 
@@ -127,5 +131,8 @@ def combined_irt():
 
 
 if __name__ == "__main__":
+    """
+    Run the desired evaluation scripts.
+    """
     # lang_irt()
-    combined_irt()
+    # combined_irt()
